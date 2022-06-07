d212587e3fc88e8e54a095325982d3e7e1416d52
Sujith175 <74652675+Sujith175@users.noreply.github.com>
2022-06-08 00:25:41 +0530

    Update db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 
 const connectDB = async() =>{
-    await mongoose.connect(process.env.MONGODB_URI,{
+    await mongoose.connect("mongodb+srv://erpweb:sujith123@cluster0.xargmoa.mongodb.net/?retryWrites=true&w=majority",{
         useUnifiedTopology: true,
     });
     console.log("mongoDb connected");
